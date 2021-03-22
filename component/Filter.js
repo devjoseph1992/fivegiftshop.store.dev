48eb587492c62227ab05f50116dbd05476f7333a
John Joseph Florina <devjosephflorina@gmail.com>
2021-03-23 07:10:23 +0800

    tablet view

--- component/Filter.js
+++ component/Filter.js
@@ -1,7 +1,7 @@
 const Filter = ({ isFilter, find}) => {
     return (
             <form className={isFilter ? 'bg-green-600' : 'hidden'}>
-                <fieldset className='px-4 border-t border-green-900'>
+                <div className='px-4 border-t border-green-900'>
                     <div className='mt-2'>
                         <label>
                             <span className='text-sm font-semibold text-white'>Price Range</span>
@@ -12,31 +12,35 @@ const Filter = ({ isFilter, find}) => {
                             </select>
                         </label>
                     </div>
-                    </fieldset>
-                    <fieldset className='px-4 mt-4 border-t border-green-900'>
+                    </div>
+                    <div className='px-4 mt-4 border-t border-green-900'>
                         <span className='block text-sm font-semibold text-white'>Category</span>
-                        <label className='flex items-center'>
-                            <input className='form-radio bg-green-700' type='radio' name='categoryType' value='boys'/>
-                            <span className='ml-2 text-white'>boys</span> 
-                        </label>
-                        <label className='flex items-center'>
-                            <input className='form-radio bg-green-700' type='radio' name='categoryType' value='girl'/>
-                            <span className='ml-2 text-white'>girl</span>    
-                        </label>    
-                    </fieldset>
-                    <fieldset className='px-4 mt-4 border-t border-green-900'>
+                        <div className='sm:flex sm:-mx-2'>
+                            <label className='flex items-center sm:w-1/4 sm:px-2'>
+                                <input className='form-radio bg-green-700' type='radio' name='categoryType' value='boys'/>
+                                <span className='ml-2 text-white'>boys</span> 
+                            </label>
+                            <label className='flex items-center sm:w-1/4 sm:px-2'>
+                                <input className='form-radio bg-green-700' type='radio' name='categoryType' value='girl'/>
+                                <span className='ml-2 text-white'>girl</span>    
+                            </label>    
+                        </div>    
+                    </div>
+                    <div className='px-4 mt-4 mb-3 border-t border-green-900'>
                         <span className='block text-sm font-semibold text-white'>Collection</span>
-                        <label className='flex items-center'>
-                            <input className='h-4 w-4 form-checkbox bg-green-700' type='checkbox' name='shoes'/>
-                            <span className='ml-2 text-white'>ball</span>
-                        </label>
-                        <label className='flex items-center'>
-                            <input className='h-4 w-4 form-checkbox bg-green-700' type='checkbox' name='shoes'/>
-                            <span className='ml-2 text-white'>shoes</span>    
-                        </label>    
-                </fieldset>
-                <div className='bg-green-800 px-4 py-4'>
-                    <button className='block w-full bg-green-700 hover:bg-green-600 font-semibold text-white px-4 py-2 rounded-lg' onClick={find}>Update filters</button>
+                        <div className='sm:flex sm:-mx-2 sm:flex-wrap'>
+                            <label className='flex items-center sm:w-1/4 sm:px-2'>
+                                <input className='h-4 w-4 form-checkbox bg-green-700' type='checkbox' name='shoes'/>
+                                <span className='ml-2 text-white'>ball</span>
+                            </label>
+                            <label className='flex items-center sm:w-1/4 sm:px-2'>
+                                <input className='h-4 w-4 form-checkbox bg-green-700' type='checkbox' name='shoes'/>
+                                <span className='ml-2 text-white'>shoes</span>    
+                            </label>
+                        </div>    
+                    </div>
+                <div className='bg-green-800 px-4 py-4 sm:text-right'>
+                    <button className='block w-full sm:w-auto sm:inline-block bg-green-700 hover:bg-green-600 font-semibold text-white px-4 py-2 rounded-lg' onClick={find}>Update filters</button>
                 </div>  
             </form>
     )
--- component/Header.js
+++ component/Header.js
@@ -4,12 +4,12 @@ import Link from 'next/link'
 
 const Header = ({ toggle }) => {
     return (
-        <header className='bg-green-700 px-4 py-3 shadow-lg border-b border-green-900'>
+        <header className='bg-green-700 px-4 py-3 shadow-lg border-b border-green-900 sm:justify-between'>
             <nav className='flex justify-between items-center relative' role='navigation'> 
                 <Link href='/'>
                     <a className='text-white font-semibold'>FiveGiftShop</a>
                 </Link>
-                <div className='px-4 cursor-pointer md:hidden' onClick={toggle}>
+                <div className='px-4 cursor-pointer sm:hidden' onClick={toggle}>
                     <svg className='h-6 w-6 text-white' xmlns="http://www.w3.org/2000/svg" fillRule="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
@@ -18,7 +18,7 @@ const Header = ({ toggle }) => {
                     {
                         navLinks.map((link, index) => (
                             <Link key={index} href={link.path}>
-                                <a className='p-4 text-white uppercase'>{link.title}</a>
+                                <a className='p-4 text-white uppercase hover:text-gray-200 sm:text-sm sm:px-2'>{link.title}</a>
                             </Link>
                         ))
                     }
